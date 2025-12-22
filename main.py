e5c79d2ea83ae1d5689625a51cba10c75f352492
GrandMarkimus <copter.smj@gmail.com>
2025-12-21 23:22:36 -0600

    Starter main file

---
+++ main.py
@@ -0,0 +1,24 @@
+import math
+
+#variables to determine
+end_a_facing = ""
+end_b_facing = ""
+
+end_a_ru = 0 #range 0 to 48
+end_b_ru = 0 #range 0 to 48
+
+rack_width_count = 0
+
+cross_aisle = ""
+
+#constants
+RU = 1.75
+RACK_WIDTH = 31.5
+RACK_HEIGHT = 89.9
+RACK_DEPTH = 47.2
+AISLE = 86.65
+
+end_a_facing = input("End-A facing (F/R)?")
+end_b_facing = input("End-B facing (F/R)?")
+cross_aisle = input("Does cable cross aisle (Y/N)?")
+
